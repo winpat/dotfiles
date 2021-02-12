6595b48b07cc903eea7cdd06024e6dee11924e57
Patrick Winter <patrickwinter@posteo.ch>
2021-02-12 16:18:41 +0100

    Use credentials to connecto media share

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -380,7 +380,7 @@
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},username=guest,password"];
+    in ["${automount_opts},credentials=/home/patrick/.samba/personal"];
   };
 
   # Enable smart card reader
