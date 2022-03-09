499779ffba14ae3af16eff8414329118a44a897f
Patrick Winter <patrickwinter@posteo.ch>
2022-03-09 12:55:54 +0100

    Enable noisetorch program (to apply CAP_SYS_RESOURCE)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -491,6 +491,8 @@
     powerOnBoot = true;
   };
 
+  programs.noisetorch.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
